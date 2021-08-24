68e338286d953b5ecc594b11c79038b29e906ad5
Lin <lhchenlin@gmail.com>
2021-08-23 22:20:37 -0400

    update view

---
+++ precache-manifest.5d6470717fa91d1dc50282e3d4e63a5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f35e3393366eaf1d491004b78cf4951",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ff67c80007a398fc568",
+    "url": "/static/css/main.94d03fdf.chunk.css"
+  },
+  {
+    "revision": "7080aa5092c389f715ea",
+    "url": "/static/js/2.ca07f204.chunk.js"
+  },
+  {
+    "revision": "1ff67c80007a398fc568",
+    "url": "/static/js/main.529ece57.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
